511e85ddd8263d50fcdd04d573ef6fd10b2c8939
tcornell95 <88857577+tcornell95@users.noreply.github.com>
2024-07-08 17:56:07 -0400

    Initial game build

---
+++ scripts/game.gd
@@ -0,0 +1,44 @@
+extends Node2D
+
+var lives = 3
+var score = 0
+
+@onready var player = $Player
+@onready var hud = $UI/HUD
+@onready var ui = $UI
+
+@onready var enemy_hit_sound = $EnemyHitSound
+@onready var player_hit_sound = $PlayerHitSound
+
+var gos_scene = preload("res://scenes/game_over_screen.tscn")
+
+func _ready():
+	hud.set_score_label(score)
+	hud.set_lives(lives)
+
+func _on_deathzone_area_entered(area):
+	area.queue_free()
+
+func _on_player_took_damage():
+	player_hit_sound.play()
+	lives -= 1
+	hud.set_lives(lives)
+	if lives == 0:
+		player.die()
+		await get_tree().create_timer(1.5).timeout
+		var gos = gos_scene.instantiate()
+		gos.set_score(score)
+		ui.add_child(gos)
+
+func _on_enemy_spawner_enemy_spawned(enemy_instance):
+	enemy_instance.connect("died", _on_enemy_died)
+	add_child(enemy_instance)
+
+func _on_enemy_died():
+	score += 100
+	hud.set_score_label(score)
+	enemy_hit_sound.play()
+
+func _on_enemy_spawner_path_enemy_spawned(path_enemy_instance):
+	add_child(path_enemy_instance)
+	path_enemy_instance.enemy.connect("died", _on_enemy_died)
---
+++ scripts/player.gd
@@ -0,0 +1,44 @@
+extends CharacterBody2D
+
+signal took_damage
+
+var speed = 300
+
+var rocket_scene = preload("res://scenes/rocket.tscn")
+
+@onready var rocket_container = $RocketContainer
+
+@onready var rocket_shot_sound = $RocketShotSound
+
+func _process(delta):
+	if Input.is_action_just_pressed("shoot"):
+		shoot()
+	
+func _physics_process(delta):
+	velocity = Vector2(0, 0)
+	if Input.is_action_pressed("move_right"):
+		velocity.x = speed
+	if Input.is_action_pressed("move_left"):
+		velocity.x = -speed
+	if Input.is_action_pressed("move_down"):
+		velocity.y = speed
+	if Input.is_action_pressed("move_up"):
+		velocity.y = -speed
+	move_and_slide()
+	
+	var screen_size = get_viewport_rect().size
+	global_position = global_position.clamp(Vector2(0,0), screen_size)
+
+
+func shoot():
+	var rocket_instance = rocket_scene.instantiate()
+	rocket_container.add_child(rocket_instance)
+	rocket_instance.global_position = global_position
+	rocket_instance.global_position.x += 80
+	rocket_shot_sound.play()
+
+func take_damage():
+	emit_signal("took_damage")
+	
+func die():
+	queue_free()
